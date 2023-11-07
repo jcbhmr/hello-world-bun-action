563638bc5c5da803d8db45af76e9b4f6c0b070a9
Jacob Hummer <jcbhmr@outlook.com>
2023-11-07 12:26:32 -0600

    try this

--- _main.mjs
+++ _main.mjs
@@ -1,27 +1,30 @@
 import { spawn } from "node:child_process";
 import { once } from "node:events";
 import { join, dirname } from "node:path";
-// TODO: Get max 30+
-const response = await fetch("https://ungh.cc/repos/oven-sh/bun/releases");
-const json = await response.json();
-const version = json.releases
-  .map((x) => x.tag)
-  .find((x) => x.startsWith("bun-v1."))
-  .match(/v(1\.\d+\.\d+)/)[1];
+import { existsSync } from "node:fs";
 const file = join(dirname(process.argv[1]), "main.ts"); // 👈 CHANGE ME!
-const subprocess = spawn(
-  `export -n version arch file
-  cache="$RUNNER_TOOL_CACHE/bun/$versin/$arch"
-  if [[ ! -d $cache ]]; then
-    curl -fsSL https://bun.sh/install \\
-      | BUN_INSTALL="$cache" bash -s "bun-v$version" &> /dev/null
-  fi
-  exec "$cache/bin/bun" "$file"`,
-  {
-    shell: "bash",
-    stdio: "inherit",
-    env: { ...process.env, version, arch: process.arch, file },
-  }
+// https://github.com/oven-sh/bun/issues/6964
+const response = await fetch(
+  "https://raw.githubusercontent.com/jcbhmr/bun-versions/main/versions.json"
 );
-await once(subprocess, "spawn");
-subprocess.on("exit", (x) => process.exit(x));
+const json = await response.json();
+const tag = json.bun.find((x) => x.startsWith("bun-v1."));
+const version = tag.slice(5);
+const BUN_INSTALL = join(
+  process.env.RUNNER_TOOL_CACHE,
+  "bun",
+  version,
+  process.arch
+);
+if (!existsSync(BUN_INSTALL)) {
+  const subprocess1 = spawn(
+    `curl -fsSL https://bun.sh/install | bash -s "$tag"`,
+    { shell: "bash", env: { ...process.env, BUN_INSTALL, tag } }
+  );
+  await once(subprocess1, "exit");
+}
+const subprocess2 = spawn(join(DENO_INSTALL, "bin", "bun"), [file], {
+  stdio: "inherit",
+});
+await once(subprocess2, "spawn");
+subprocess2.on("exit", (x) => process.exit(x));
